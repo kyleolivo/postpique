e3800db19cf04730eb377351408824e6dc85cbda
Kyle Olivo <kyle@kyleolivo.com>
2025-07-26 14:53:39 -0700

    Initial Commit

---
+++ PostPique/PostPiqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostPiqueApp.swift
+//  PostPique
+//
+//  Created by Kyle Olivo on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct PostPiqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
